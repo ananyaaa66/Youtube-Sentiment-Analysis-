f428185042f6d9e89729b803acba39fa16e954ac
ananyaaa66 <singhananya.900567@gmail.com>
2025-10-07 00:19:24 +0530

    fully ran dvc and registered model

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -6,7 +6,7 @@ import logging
 import os
 
 # Set up MLflow tracking URI
-mlflow.set_tracking_uri("http://ec2-54-167-108-249.compute-1.amazonaws.com:5000/")
+mlflow.set_tracking_uri("http://ec2-16-171-193-73.eu-north-1.compute.amazonaws.com:5000/")
 
 
 # logging configuration
